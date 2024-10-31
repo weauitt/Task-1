f9e161a74d462405077327f6b874746ac8583867
wequit <adilhanimasev6@gmail.com>
2024-10-31 04:49:46 +0300

    Improvements

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,79 @@
-import Image from "next/image";
+"use client";
+import { startDate, endDate } from "./api/dates";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [isCountdownToEnd, setIsCountdownToEnd] = useState(false);
+  const [isFinished, setIsFinished] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const updateCountdown = (difference: number) => {
+    const d = Math.floor(difference / (1000 * 60 * 60 * 24));
+    setDays(d);
+
+    const h = Math.floor(
+      (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    setHours(h);
+
+    const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    setMinutes(m);
+
+    const s = Math.floor((difference % (1000 * 60)) / 1000);
+    setSeconds(s);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+
+      if (isFinished) return;
+
+      if (!isCountdownToEnd && now < startDate) {
+        const difference = startDate.getTime() - now.getTime();
+        updateCountdown(difference);
+      } else if (!isCountdownToEnd && now >= startDate) {
+        setIsCountdownToEnd(true);
+      } else if (isCountdownToEnd && now < endDate) {
+        const difference = endDate.getTime() - now.getTime();
+        updateCountdown(difference);
+      } else {
+        setIsFinished(true);
+        clearInterval(interval);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [isCountdownToEnd, isFinished]);
+
+  return (
+    <div className="flex justify-center items-center gap-10 text-black">
+      <h3 className="text-center text-4xl">
+        {isFinished ? "ВРЕМЯ ЗАКОНЧИЛОСЬ" : "ОСТАЛОСЬ ВРЕМЕНИ"}
+      </h3>
+      <section className="flex justify-center items-center h-screen">
+        <div className="flex space-x-4">
+          <div className="text-center">
+            <span className="block text-2xl">{days}</span>
+            <span className="text-sm">День</span>
+          </div>
+          <div className="text-center">
+            <span className="block text-2xl">{hours}</span>
+            <span className="text-sm">Час</span>
+          </div>
+          <div className="text-center">
+            <span className="block text-2xl">{minutes}</span>
+            <span className="text-sm">Минут</span>
+          </div>
+          <div className="text-center">
+            <span className="block text-2xl">{seconds}</span>
+            <span className="text-sm">Секунд</span>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </section>
     </div>
   );
 }
